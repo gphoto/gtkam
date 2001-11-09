e0fa8cf261bc0373bdb4500402cea450211d00b1
Lutz Müller <lutz@topfrose.de>
2001-11-09 13:33:24 +0000

    Implement rotation by 180°.
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/gtkam@2974 67ed7778-7388-44ab-90cf-0a291f65f57c

--- src/gtkam-preview.c
+++ src/gtkam-preview.c
@@ -48,6 +48,7 @@
 #include <gtk/gtkhbox.h>
 #include <gtk/gtkcheckbutton.h>
 #include <gtk/gtkspinbutton.h>
+#include <gtk/gtkradiobutton.h>
 #include <gtk/gtkbutton.h>
 #include <gtk/gtkmain.h>
 #include <gtk/gtksignal.h>
@@ -60,9 +61,12 @@
 
 struct _GtkamPreviewPrivate
 {
-	GtkWidget *hbox, *preview, *spin;
+	GtkWidget *spin;
+	GtkPixmap *image;
 
 	Camera *camera;
+
+	guint rotate;
 
 	guint32 timeout;
 	guint32 timeout_id;
@@ -184,6 +188,72 @@ on_preview_capture_clicked (GtkButton *button, GtkamPreview *preview)
 	}
 }
 
+struct _GdkPixbuf {
+	int ref_count;
+	GdkColorspace colorspace;
+	int n_channels;
+	int bits_per_sample;
+	int width, height;
+	int rowstride;
+	guchar *pixels;
+	GdkPixbufDestroyNotify destroy_fn;
+	gpointer destroy_fn_data;
+	GdkPixbufLastUnref last_unref_fn;
+	gpointer last_unref_fn_data;
+	guint has_alpha : 1;
+};
+
+#define SWAP(c1,c2) {guchar c = (c1); (c1) = (c2); (c2) = c;}
+
+static void
+gdk_pixbuf_rotate (GdkPixbuf *pixbuf, guint angle)
+{
+	guint row, col, row_len;
+	guint channels;
+
+	g_return_if_fail (pixbuf != NULL);
+
+	channels = pixbuf->has_alpha ? 4 : 3;
+	row_len = pixbuf->width * channels;
+
+	switch (angle) {
+	case 0:
+		break;
+	case 90:
+		break;
+	case 180:
+		for (row = 0; row < pixbuf->height / 2; row++) {
+			for (col = 0; col < pixbuf->width; col++) {
+SWAP (pixbuf->pixels[row * row_len + col * channels + 0],
+      pixbuf->pixels[(pixbuf->height - row) * row_len - col * channels + 0]);
+SWAP (pixbuf->pixels[row * row_len + col * channels + 1],
+      pixbuf->pixels[(pixbuf->height - row) * row_len - col * channels + 1]);
+SWAP (pixbuf->pixels[row * row_len + col * channels + 2],
+      pixbuf->pixels[(pixbuf->height - row) * row_len - col * channels + 2]);
+			}
+		}
+		if (pixbuf->height % 2) {
+			for (col = 0; col < pixbuf->width / 2; col++) {
+SWAP (pixbuf->pixels[row * row_len + col * channels + 0],
+      pixbuf->pixels[(pixbuf->height - row) * row_len - col * channels + 0]);
+SWAP (pixbuf->pixels[row * row_len + col * channels + 1],
+      pixbuf->pixels[(pixbuf->height - row) * row_len - col * channels + 1]);
+SWAP (pixbuf->pixels[row * row_len + col * channels + 2],
+      pixbuf->pixels[(pixbuf->height - row) * row_len - col * channels + 2]);
+			}
+		}
+
+		break;
+	case 270:
+		break;
+	default:
+		g_warning ("Rotation by %i not implemented.", angle);
+		break;
+	}
+}
+
+#undef SWAP
+
 static gboolean
 timeout_func (gpointer user_data)
 {
@@ -214,22 +284,19 @@ timeout_func (gpointer user_data)
 	gdk_pixbuf_loader_close (loader);
 
 	pixbuf = gdk_pixbuf_loader_get_pixbuf (loader);
+	gdk_pixbuf_rotate (pixbuf, preview->priv->rotate);
 	gdk_pixbuf_render_pixmap_and_mask (pixbuf, &pixmap, &bitmap, 1);
 	gtk_object_unref (GTK_OBJECT (loader));
 
 	if (!GTKAM_IS_PREVIEW (preview))
 		return (FALSE);
 
-	/* Destroy old preview */
-	if (preview->priv->preview)
-		gtk_container_remove (GTK_CONTAINER (preview->priv->hbox),
-				      preview->priv->preview);
-
 	/* Show the new preview */
-	preview->priv->preview = gtk_pixmap_new (pixmap, bitmap);
-	gtk_widget_show (preview->priv->preview);
-	gtk_box_pack_start (GTK_BOX (preview->priv->hbox),
-			    preview->priv->preview, TRUE, TRUE, 0);
+	gtk_pixmap_set (preview->priv->image, pixmap, bitmap);
+	if (pixmap)
+		gdk_pixmap_unref (pixmap);
+	if (bitmap)
+		gdk_bitmap_unref (bitmap);
 
 	return (TRUE);
 }
@@ -237,6 +304,9 @@ timeout_func (gpointer user_data)
 static void
 on_refresh_toggled (GtkToggleButton *toggle, GtkamPreview *preview)
 {
+	g_return_if_fail (GTK_IS_TOGGLE_BUTTON (toggle));
+	g_return_if_fail (GTKAM_IS_PREVIEW (preview));
+
 	if (!toggle->active && preview->priv->timeout_id) {
 		gtk_timeout_remove (preview->priv->timeout_id);
 		preview->priv->timeout_id = 0;
@@ -256,12 +326,56 @@ on_adjustment_value_changed (GtkAdjustment *adj, GtkamPreview *preview)
 	preview->priv->timeout = adj->value * 1000;
 }
 
+static void
+on_radio_0_toggled (GtkToggleButton *toggle, GtkamPreview *preview)
+{
+	g_return_if_fail (GTK_IS_TOGGLE_BUTTON (toggle));
+	g_return_if_fail (GTKAM_IS_PREVIEW (preview));
+
+	if (toggle->active)
+		preview->priv->rotate = 0;
+}
+
+static void
+on_radio_90_toggled (GtkToggleButton *toggle, GtkamPreview *preview)
+{
+	g_return_if_fail (GTK_IS_TOGGLE_BUTTON (toggle));
+	g_return_if_fail (GTKAM_IS_PREVIEW (preview));
+
+	if (toggle->active)
+		preview->priv->rotate = 90;
+}
+
+static void
+on_radio_180_toggled (GtkToggleButton *toggle, GtkamPreview *preview)
+{
+	g_return_if_fail (GTK_IS_TOGGLE_BUTTON (toggle));
+	g_return_if_fail (GTKAM_IS_PREVIEW (preview));
+
+	if (toggle->active)
+		preview->priv->rotate = 180;
+}
+
+static void
+on_radio_270_toggled (GtkToggleButton *toggle, GtkamPreview *preview)
+{
+	g_return_if_fail (GTK_IS_TOGGLE_BUTTON (toggle));
+	g_return_if_fail (GTKAM_IS_PREVIEW (preview));
+
+	if (toggle->active)
+		preview->priv->rotate = 270;
+}
+
 GtkWidget *
 gtkam_preview_new (Camera *camera)
 {
 	GtkamPreview *preview;
-	GtkWidget *button, *check, *label, *hbox;
+	GtkWidget *button, *check, *label, *hbox, *image, *vbox, *radio;
 	GtkObject *adj;
+	GdkPixbuf *pixbuf;
+	GdkPixmap *pixmap;
+	GdkBitmap *bitmap;
+	GSList *group;
 
 	g_return_val_if_fail (camera != NULL, NULL);
 
@@ -270,11 +384,66 @@ gtkam_preview_new (Camera *camera)
 	preview->priv->camera = camera;
 	gp_camera_ref (camera);
 
-	preview->priv->hbox = gtk_hbox_new (FALSE, 0);
-	gtk_container_set_border_width (GTK_CONTAINER (preview->priv->hbox), 5);
-	gtk_widget_show (preview->priv->hbox);
+	hbox = gtk_hbox_new (FALSE, 0);
+	gtk_container_set_border_width (GTK_CONTAINER (hbox), 5);
+	gtk_widget_show (hbox);
 	gtk_box_pack_start (GTK_BOX (GTK_DIALOG (preview)->vbox),
-			    preview->priv->hbox, TRUE, TRUE, 0);
+			    hbox, TRUE, TRUE, 0);
+
+	pixbuf = gdk_pixbuf_new_from_file (IMAGE_DIR "/gtkam-camera.png");
+	if (!pixbuf)
+		g_warning ("Could not load " IMAGE_DIR "/gtkam-camera.png");
+	else {
+		gdk_pixbuf_render_pixmap_and_mask (pixbuf, &pixmap, &bitmap, 127);
+		gdk_pixbuf_unref (pixbuf);
+		image = gtk_pixmap_new (pixmap, bitmap);
+		if (pixmap)
+			gdk_pixmap_unref (pixmap);
+		if (bitmap)
+			gdk_bitmap_unref (bitmap);
+		gtk_widget_show (image);
+		gtk_box_pack_start (GTK_BOX (hbox), image, FALSE, FALSE, 0);
+	}
+
+	/* Empty pixbuf (for preview) */
+	pixbuf = gdk_pixbuf_new (GDK_COLORSPACE_RGB, FALSE, 8, 80, 60);
+	gdk_pixbuf_render_pixmap_and_mask (pixbuf, &pixmap, &bitmap, 127);
+	gdk_pixbuf_unref (pixbuf); 
+	image = gtk_pixmap_new (pixmap, bitmap);
+	if (pixmap) 
+		gdk_pixmap_unref (pixmap); 
+	if (bitmap) 
+		gdk_bitmap_unref (bitmap);
+	gtk_widget_show (image); 
+	gtk_box_pack_start (GTK_BOX (hbox), image, TRUE, TRUE, 0);
+	preview->priv->image = GTK_PIXMAP (image);
+
+	/* Rotate */
+	vbox = gtk_vbox_new (FALSE, 0);
+	gtk_widget_show (vbox);
+	gtk_box_pack_end (GTK_BOX (hbox), vbox, FALSE, FALSE, 0);
+	radio = gtk_radio_button_new_with_label (NULL, _("0°"));
+	gtk_widget_show (radio);
+	gtk_box_pack_start (GTK_BOX (vbox), radio, FALSE, FALSE, 0);
+	gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (radio), TRUE);
+	gtk_signal_connect (GTK_OBJECT (radio), "toggled",
+			    GTK_SIGNAL_FUNC (on_radio_0_toggled), preview);
+	group = gtk_radio_button_group (GTK_RADIO_BUTTON (radio));
+	radio = gtk_radio_button_new_with_label (group, _("-90°"));
+//	gtk_widget_show (radio);
+	gtk_box_pack_start (GTK_BOX (vbox), radio, FALSE, FALSE, 0);
+	gtk_signal_connect (GTK_OBJECT (radio), "toggled",
+			    GTK_SIGNAL_FUNC (on_radio_270_toggled), preview);
+	radio = gtk_radio_button_new_with_label (group, _("+90°"));
+//	gtk_widget_show (radio);
+	gtk_box_pack_start (GTK_BOX (vbox), radio, FALSE, FALSE, 0);
+	gtk_signal_connect (GTK_OBJECT (radio), "toggled",
+			    GTK_SIGNAL_FUNC (on_radio_90_toggled), preview);
+	radio = gtk_radio_button_new_with_label (group, _("180°"));
+	gtk_widget_show (radio);
+	gtk_box_pack_start (GTK_BOX (vbox), radio, FALSE, FALSE, 0);
+	gtk_signal_connect (GTK_OBJECT (radio), "toggled",
+			    GTK_SIGNAL_FUNC (on_radio_180_toggled), preview);
 
 	hbox = gtk_hbox_new (FALSE, 0);
 	gtk_container_set_border_width (GTK_CONTAINER (hbox), 5);
@@ -313,10 +482,10 @@ gtkam_preview_new (Camera *camera)
 			   button);
 	gtk_widget_grab_focus (button);
 
-	button = gtk_button_new_with_label (_("Cancel"));
+	button = gtk_button_new_with_label (_("Close"));
 	gtk_widget_show (button);
 	gtk_signal_connect (GTK_OBJECT (button), "clicked",
-			    GTK_SIGNAL_FUNC (on_preview_close_clicked), preview);
+		GTK_SIGNAL_FUNC (on_preview_close_clicked), preview);
 	gtk_container_add (GTK_CONTAINER (GTK_DIALOG (preview)->action_area),
 			   button);
 

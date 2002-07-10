d5553d5e742da961dc8c880907a32ed876ea3cec
Lutz Müller <lutz@topfrose.de>
2002-07-10 12:36:15 +0000

    2002-07-10  Lutz Müller <lutz@users.sourceforge.net>
    	Patch by Arnaud Launay <asl@launay.org>:
    
    	* autogen.sh: Check for pkg-config.
    
    
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/gtkam@4840 67ed7778-7388-44ab-90cf-0a291f65f57c

--- autogen.sh
+++ autogen.sh
@@ -32,6 +32,14 @@ PROJECT=gtkam
 	DIE=1
 }
 
+(pkg-config --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "**Error**: You must have \`pkg-config installed."
+	echo "Download the appropriate package for your distribution,"
+	echo "or get the source tarball at http://www.freedesktop.org/software/pkgconfig/"
+	DIE=1
+}
+
 if test "$DIE" -eq 1; then
 	exit 1
 fi

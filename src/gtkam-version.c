f440d821ebb345fc9d1c847c57f84c1ff1fe5b05
Hans Ulrich Niedermann <hun@n-dimensional.de>
2002-11-22 18:10:09 +0000

    added version/feature stuff
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/branches/gtkam-0_1_10/gtkam@5748 67ed7778-7388-44ab-90cf-0a291f65f57c

---
+++ src/gtkam-version.c
@@ -0,0 +1,100 @@
+/* version.c
+ *
+ * Copyright (C) 2002 Hans Ulrich Niedermann <gp@n-dimensional.de
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details. 
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include <config.h>
+
+#include <stdlib.h>
+
+#include "gtkam-version.h"
+
+static const char **gtkam_frontend_version(GPVersionVerbosity verbose)
+{
+	/* we could also compute/parse the short strings from the long
+	   ones, but the current method is easier for now :-) */
+	static const char *verb[] = {
+		VERSION,
+#ifdef HAVE_CC
+		HAVE_CC " (C compiler used)",
+#endif
+#ifdef HAVE_BONOBO
+		"bonobo (for FIXME)",
+#else
+		"no bonobo (for FIXME)",
+#endif
+#ifdef HAVE_GNOME
+		"gnome (for FIXME)",
+#else
+		"no gnome (for FIXME)",
+#endif
+#ifdef HAVE_GIMP
+		"gimp (plugin for The Gimp)",
+#else
+		"no gimp (plugin for The Gimp)",
+#endif
+#ifdef HAVE_EXIF
+		"exif (for displaying exif information)",
+#else
+		"no exif (for displaying exif information)",
+#endif
+		NULL
+	};
+	static const char *shrt[] = {
+		VERSION,
+#ifdef HAVE_CC
+		HAVE_CC,
+#endif
+#ifdef HAVE_BONOBO
+		"bonobo (for FIXME)",
+#else
+		"no bonobo (for FIXME)",
+#endif
+#ifdef HAVE_GNOME
+		"gnome (for FIXME)",
+#else
+		"no gnome (for FIXME)",
+#endif
+#ifdef HAVE_GIMP
+		"gimp (plugin for The Gimp)",
+#else
+		"no gimp (plugin for The Gimp)",
+#endif
+#ifdef HAVE_EXIF
+		"exif (for displaying exif information)",
+#else
+		"no exif (for displaying exif information)",
+#endif
+		NULL
+	};
+	return((verbose == GP_VERSION_VERBOSE)?verb:shrt);
+}
+
+const module_version module_versions[] = {
+  	{ "gtkam", gtkam_frontend_version },
+  	{ "libgphoto2", gp_library_version },
+	{ "libgphoto2_port", gp_port_library_version },
+	{ NULL, NULL }
+};
+
+/*
+ * Local Variables:
+ * c-file-style:"linux"
+ * indent-tabs-mode:t
+ * End:
+ */

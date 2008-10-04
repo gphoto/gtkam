f85143a71ecfdf19e8d9734578a1a5f74d2c66a4
Marcus Meissner <marcus@jet.franken.de>
2008-10-04 14:37:12 +0000

    adjust copyright headers to match GPL. added developers + year copyright lines.
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/gtkam@11363 67ed7778-7388-44ab-90cf-0a291f65f57c

--- src/gtkam-preview.c
+++ src/gtkam-preview.c
@@ -1,21 +1,20 @@
 /* gtkam-preview.c
  *
- * Copyright © 2001 Lutz Müller <lutz@users.sf.net>
+ * Copyright 2001,2002 Lutz Mueller <lutz@users.sf.net>
+ * Copyright 2002,2003 Patrick Mansfield <patmans@users.sourceforge.net>
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details. 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "config.h"
